32f75e199b0773c53dbf41a47441ad08bf455237
Jon1Games <jonas@salosna.com>
2024-12-30 11:27:45 +0100

    dice, disconnect, random number

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import os
 from dotenv import load_dotenv
 import mariadb
 import sys
-from datetime import datetime, timedelta, date
-import threading
-import asyncio
+from datetime import datetime, timedelta
 from discord.ext import tasks
+import json
+from random import randrange, randint
+
+config = json.load("config.json")
 
 intents = discord.Intents.default()
 intents.members = True
@@ -104,11 +106,11 @@ recalcRoleWarnCound.start()
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
-@bot.slash_command(name="ping", description="reply with pong")
+@bot.slash_command(name="ping", description="Antwortet mit \"Pong!\"")
 async def ping(ctx: discord.ApplicationContext):
-    await ctx.respond("pong")
+    await ctx.respond("Pong!", ephemeral=True)
 
-@bot.slash_command(name="warn")
+@bot.slash_command(name="warn", description="Verwarnt ein Benutzer.")
 @discord.default_permissions(
     administrator=True,
 )
@@ -167,22 +169,22 @@ async def warn(ctx: discord.ApplicationContext, user, reason: str, hours: int, d
         embed = discord.Embed(title=f"__**EIn Fehler ist aufgetreten, bitte Kontaktiere Jon1Games*__", color=0xFF0000)
         await ctx.respond(embed=embed)
 
-@bot.slash_command(name="list_warns")
+@bot.slash_command(name="warn list", description="Erstellt eine Liste mit Verwarnungen.")
 @discord.default_permissions(
     administrator=True,
 )
 @discord.option(
-    "user",
+    "nutzer",
     discord.User,
     required=False,
     default=''
 )
 @discord.option(
-    "page",
+    "seite",
     required=False,
     default='1'
 )
-async def list_warns(ctx: discord.ApplicationContext, user, page):
+async def warn_list(ctx: discord.ApplicationContext, nutzer, seite):
     await recalcRoleWarnCound()
     wait_embed = discord.Embed(title=f"__**Generating Warning list ...**__", color=0x03f8fc)
     msg = await ctx.respond(embed=wait_embed)
@@ -190,7 +192,7 @@ async def list_warns(ctx: discord.ApplicationContext, user, page):
     now = datetime.now()
     conn = dbConnect()
     cur = conn.cursor()
-    if user == "":
+    if nutzer == "":
         cur.execute("SELECT `id`,`guild`,`user`,`mod`,`reason`, `exipire` FROM `warnings` WHERE (guild=? AND (exipire>? or exipire IS NULL));", (ctx.guild_id, int(now.strftime("%Y%m%d%H"))))
         guildmode = True
     else: 
@@ -200,11 +202,11 @@ async def list_warns(ctx: discord.ApplicationContext, user, page):
     if guildmode == True:
         u = ctx.guild.name
     else: 
-        u = user
+        u = nutzer
     embed = discord.Embed(title=f"__**Warning list of {u}**__", color=0x03f8fc)
 
-    offset = int(page) * 10
-
+    offset = int(seite) * 10
+    
     i = 1
     for id,guild,user,mod,reason,exipire in cur:
         if i <= offset - 10:
@@ -214,11 +216,11 @@ async def list_warns(ctx: discord.ApplicationContext, user, page):
             n = await bot.fetch_user(mod)
             if guildmode:
                 if exipire == None:
-                    u = await bot.fetch_user(user)
+                    u = await bot.fetch_user(nutzer)
                     embed.add_field(name=f'**ID: {id}**', value=f'> Nutzer: {u.mention}\n> Moderator: {n}\n> Grund: {reason}',inline=False)
                 else:
                     time = str(exipire)
-                    u = await bot.fetch_user(user)
+                    u = await bot.fetch_user(nutzer)
                     embed.add_field(name=f'**ID: {id}**', value=f'> Nutzer: {u.mention}\n> Moderator: {n}\n> Grund: {reason}\n> Auslauf Datum: {time[0]}{time[1]}{time[2]}{time[3]}/{time[4]}{time[5]}/{time[6]}{time[7]}, {time[8]}{time[9]}:00',inline=False)
             else:
                 if exipire == None:
@@ -229,7 +231,7 @@ async def list_warns(ctx: discord.ApplicationContext, user, page):
 
     await msg.edit(embed=embed)
 
-@bot.slash_command(name="remove_warn")
+@bot.slash_command(name="warn remove", description="Endfernt eine Verwarnun.")
 @discord.default_permissions(
     administrator=True,
 )
@@ -238,7 +240,7 @@ async def list_warns(ctx: discord.ApplicationContext, user, page):
     required=True,
     default=''
 )
-async def remove_warn(ctx: discord.ApplicationContext, warn_id):
+async def warn_remove(ctx: discord.ApplicationContext, warn_id):
     try: 
         conn = dbConnect()
         cur = conn.cursor()
@@ -256,7 +258,179 @@ async def remove_warn(ctx: discord.ApplicationContext, warn_id):
         conn.close()
     except mariadb.Error as e: 
         print(f"Error: {e}")
-        embed = discord.Embed(title=f"__**EIn Fehler ist aufgetreten, bitte Kontaktiere Jon1Games*__", color=0xFF0000)
+        embed = discord.Embed(title=f"__**Ein Fehler ist aufgetreten, bitte Kontaktiere Jon1Games*__", color=0xFF0000)
         await ctx.respond(embed=embed)
+
+@bot.slash_command(name="clear", description="Löscht eine Anzahl von Nachrichtem in dem Aktuellem Kanal.")
+@discord.default_permissions(
+    administrator=True,
+)
+@discord.option(
+    "count", 
+    required=True,
+    default=1
+)
+@discord.option(
+    "user", 
+    discord.User,
+    required=False,
+    default=None
+)
+async def clear(ctx: discord.ApplicationContext, count, user):
+    embed = discord.Embed(title=f"__**Bereite Löschen von {str(count)} Nachrichten vor...*__", color=0xAAFF00)
+    msg = ctx.respond(embed=embed)
+    
+    channel = ctx.channel()
+    messages = await channel.history(limit=count + 1)
+    if user is not None:
+        for a in messages:
+            if a.author.id == user.id:
+                messages.remove(a)
+    
+    for a in messages:
+        a.delete()
+    embed2 = discord.Embed(title=f"__**{str(len(messages))} Nachrichten wurden gelöscht.*__", color=0xAAFF00)
+    msg.edit(embed=embed2)
+    
+@bot.slash_command(name="lock", description="Schließt einen Kanal für eine Rolle.")
+@discord.default_permissions(
+    administrator=True,
+)
+@discord.option(
+    "role", 
+    discord.Role,
+    required=False,
+    default=None
+)
+async def lock(ctx: discord.ApplicationContext, role):
+    channel = ctx.channel()
+    if role is None:
+        role = ctx.guild.default_role
+    
+    perms = ctx.channel.overwrites_for(role)
+    perms.send_messages=False
+    
+    embed = discord.Embed(title=f"__**Kanal {channel.mention} wurde für {role.mention} gespert.**__", color=0xAAFF00)
+    msg = ctx.respond(embed=embed)
+    
+@bot.slash_command(name="unlock", description="Eröffnet einen Kanal für eine Rolle.")
+@discord.default_permissions(
+    administrator=True,
+)
+@discord.option(
+    "role", 
+    discord.Role,
+    required=False,
+    default=None
+)
+async def unlock(ctx: discord.ApplicationContext, role):
+    channel = ctx.channel()
+    if role is None:
+        role = ctx.guild.default_role
+    
+    perms = ctx.channel.overwrites_for(role)
+    perms.send_messages=True
+    
+    embed = discord.Embed(title=f"__**Kanal {channel.mention} wurde für {role.mention} entgespert.**__", color=0xAAFF00)
+    msg = ctx.respond(embed=embed)
+
+@bot.slash_command(name="random number", description="Generiert eine Nummer zwischen zwei Werten.")
+@discord.option(
+    "number1", 
+    required=False,
+    default=0
+)
+@discord.option(
+    "number2", 
+    required=False,
+    default=2147483647
+)
+async def random_number(ctx: discord.ApplicationContext, number1, number2):
+    embed = discord.Embed(title=f"__**Generiere Zahl Zwischen {str(number1)} und {str(number2)}...*__", color=0xAAFF00)
+    msg = ctx.respond(embed=embed)
+    
+    rand = randrange(number1, number2)
+    
+    embed = discord.Embed(title=f"__** {str(rand)} **__", color=0xAAFF00)
+    msg.edit(embed=embed)
+
+@bot.slash_command(name="dice", description="Würfelt in der angegebenem Formel und gibt das Ergebnis aus. (<Anzahl>D<Seiten>,<Anzahl>D<Seiten>,...)")
+@discord.option(
+    "formular", 
+    required=False,
+    default="1D6"
+)
+async def dice(ctx: discord.ApplicationContext, formular):
+    embed = discord.Embed(title=f"__**Generiere deinen Wurf...**__", color=0xAAFF00)
+    msg = await ctx.respond(embed=embed)
+    
+    def dice(formula):
+        formula = formula.split("D")
+        
+        count = 0
+        for a in formula:
+            formula[count] = int(formula[count])
+            count += 1
+        
+        dices = []
+        result = 0
+        for a in range(formula[0]):
+            rand = randrange(1, formula[1])
+            dices.append(rand)
+            result += rand
+        return dices, result
+
+    def multiDice(formula):
+        formula = formula.split(",")
+        
+        dices = []
+        result = 0
+        for a in formula:
+            dices.append(str(a))
+            d, r = dice(a)
+            dices.append(str(d))
+            result += r
+        return dices, result
+
+    dices, result = multiDice(formula=formular)
+    embed = discord.Embed(title=f"__**Wurf von {ctx.author.mention}**__", color=0xAAFF00)
+    form = None
+    for a in dices:
+        if form is None:
+            form = a
+        else:
+            embed.add_field(name=f'**{form}**', value=f'{str(a)}', inline=False)
+            form = None
+    embed.add_field(name=f'**Gesamt Ergebnis:**', value=f'{str(result)}', inline=False)
+    
+    msg.edit(embed=embed)
+
+@bot.slash_command(name="disconnect", description="Wift einen Nutzer aus seiem Aktuellem Sprachkannal.")
+@discord.default_permissions(
+    administrator=True,
+)
+@discord.option(
+    "user",
+    discord.User, 
+    required=True,
+    default=None
+)
+@discord.option(
+    "reason", 
+    required=True,
+    default=None
+)
+async def disconnect(ctx: discord.ApplicationContext, user, reason):
+    if user is None:
+            embed = discord.Embed(title=f"__**Gebe einen Nutzer an um diesen auf seinem Sprachkanal zu werfen!**__", color=0xFF0000)
+    elif user.voice is None:
+        embed = discord.Embed(title=f"__**Der Nutzer {user.mention} befindet sich nicht in einem Sprachkanal!**__", color=0xFF0000)
+    else:
+        duration = datetime.timedelta(seconds=1)
+        reason = f"/disconnect - by {user.mention} | GamingLoungeBot\n{reason}"
+        embed = discord.Embed(title=f"__**Der Nutzer {user.mention} wurde aus seinem Sprachkanal geworfen.**__", color=0xAAFF00)
+        await user.timeout(duration, reason=reason)
+        
+    msg = ctx.respond(embed=embed, ephemeral=True)
 
 bot.run(os.getenv('TOKEN'))
